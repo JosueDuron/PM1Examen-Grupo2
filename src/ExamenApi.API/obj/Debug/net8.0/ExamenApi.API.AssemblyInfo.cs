fd6839afa6a579ce21b4e8670c23d07b5bf922ae
JosueDuron <duronjosue189@gmail.com>
2026-07-14 16:28:50 -0600

    DockerFiler
    Para despliegue de API

--- src/ExamenApi.API/obj/Debug/net8.0/ExamenApi.API.AssemblyInfo.cs
+++ src/ExamenApi.API/obj/Debug/net8.0/ExamenApi.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamenApi.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fccfbbc636a7b2b9e1d89ae757bd6d569e7f7dff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamenApi.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamenApi.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ExamenApi.Domain/obj/Debug/net8.0/ExamenApi.Domain.AssemblyInfo.cs
+++ src/ExamenApi.Domain/obj/Debug/net8.0/ExamenApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamenApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fccfbbc636a7b2b9e1d89ae757bd6d569e7f7dff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamenApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamenApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
